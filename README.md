f58a233bd5bcf605707977c80c3bc7533e7b3b19
Gian Spadafora <gianspadafora@Gians-MacBook-Pro-2.local>
2025-02-06 23:52:07 +0100

    Create and run docker compose

--- README.md
+++ README.md
@@ -20,3 +20,24 @@ To do for PoC:
 1. Generate JSON with internal representation
 1. Generate Dockerfile and run it
 1. Have a way to read secrets somehow and actually deploy it
+
+current task: 
+  1. Add random generator dependency to create a random username and password for postgres when generating the docker compose.
+  2. Generate the docker compose file. 
+  
+  After POC:
+
+1. Migrate to using a webserver to generate the JSON and to start the deployment of custom resources. The other one we have to think about but this is the general talk:
+
+    The PROBLEM: we have to run their code after the json has been generated and our initial design didn't account for that
+
+    a. One way would be to run their code multiples times with different goals. One to generate the json and further times to generate each of the cloud resources.
+
+    b. Instead of the user program spitting out a json, it runs a webserver, one endpoint returns the json. Another endpoint runs the code for custom resources by id
+
+    c. Register custom resources in a different global var and figure out how to run the custom resource locally. 
+
+
+2. Migrate to docker directly instead of docker compose. Because:
+ - more flexible to start things not on docker. 
+ - if we provide a web console we will want to display the logs for the separate services running on separate tabs or something
--- lif/lif.go
+++ lif/lif.go
@@ -1,16 +1,31 @@
 package lif
 
 import (
+	"crypto/rand"
 	"encoding/json"
+	"fmt"
+	"math/big"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"gopkg.in/yaml.v3"
 )
 
-var resources = make(map[string]any)
+//BLABLABLABLABLABLABLABLABLA
+
+var (
+	resources = make(map[string]any)
+	// stores concrete values for compose local deployment
+	composeResources = make(map[string]map[string]any)
+)
 
 type AwsEC2 struct {
-	name    string
-	ports   []string
-	envVars map[string]string
+	name       string
+	ports      []string
+	envVars    map[string]string
+	dockerfile string
 }
 
 type AwsEC2WithPorts struct {
@@ -21,14 +36,19 @@ type AwsEC2WithEnvVars struct {
 	instance *AwsEC2
 }
 
+type AwsEC2WithDockerfile struct {
+	instance *AwsEC2
+}
+
 type AwsEC2Exposes struct {
 }
 
 func NewAwsEC2(name string) *AwsEC2 {
 	return &AwsEC2{
-		name:    name,
-		ports:   make([]string, 0),
-		envVars: make(map[string]string),
+		name:       name,
+		ports:      make([]string, 0),
+		envVars:    make(map[string]string),
+		dockerfile: "",
 	}
 }
 
@@ -42,15 +62,32 @@ func (e *AwsEC2) OpenPorts(ports []string) *AwsEC2WithPorts {
 func (e *AwsEC2WithPorts) WithEnvVars(envVars map[string]string) *AwsEC2WithEnvVars {
 	e.instance.envVars = envVars
 
-	resources[e.instance.name] = map[string]any{
-		"type":     "ec2",
-		"ports":    e.instance.ports,
-		"env_vars": e.instance.envVars,
-	}
-
 	return &AwsEC2WithEnvVars{
 		instance: e.instance,
 	}
+}
+
+func (e *AwsEC2WithEnvVars) PathToDockerfile(path string) (*AwsEC2WithDockerfile, error) {
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		return nil, err
+	}
+	e.instance.dockerfile = absPath
+
+	resources[e.instance.name] = map[string]any{
+		"type":       "ec2",
+		"ports":      e.instance.ports,
+		"env_vars":   e.instance.envVars,
+		"dockerfile": e.instance.dockerfile,
+	}
+
+	return &AwsEC2WithDockerfile{
+		instance: e.instance,
+	}, nil
+}
+
+type NeonPostgres struct {
+	Exposes *NeonPostgresExposes
 }
 
 type NeonPostgresExposes struct {
@@ -60,15 +97,18 @@ type NeonPostgresExposes struct {
 }
 
 func NewNeonPostgres(name string) NeonPostgresExposes {
-	resources[name] = map[string]any{
-		"type": "postgres",
-	}
-
-	return NeonPostgresExposes{
+	exposes := NeonPostgresExposes{
 		Url:      "ref:" + name + ":url",
 		User:     "ref:" + name + ":user",
 		Password: "ref:" + name + ":password",
 	}
+
+	resources[name] = map[string]any{
+		"type":    "postgres",
+		"exposes": exposes,
+	}
+
+	return exposes
 }
 
 func LifBuild() {
@@ -90,4 +130,205 @@ func LifBuild() {
 	if err != nil {
 		panic(err)
 	}
+
+	// Generate the docker compose file
+	dockerComposeBytes, err := GenerateDockerCompose(jsonBytes)
+	if err != nil {
+		panic(err)
+	}
+
+	// create temp directory for docker-compose
+	tempDir, err := os.MkdirTemp("", "lif-compose-")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	composePath := filepath.Join(tempDir, "docker-compose.yml")
+	if err := os.WriteFile(composePath, dockerComposeBytes, 0644); err != nil {
+		panic(err)
+	}
+
+	// Run docker compose up
+	cmd := exec.Command("docker", "compose", "-f", composePath, "up")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+}
+
+type DockerCompose struct {
+	Version  string                    `yaml:"version"`
+	Services map[string]map[string]any `yaml:"services"`
+}
+
+func GenerateDockerCompose(jsonBytes []byte) ([]byte, error) {
+	// parse the json
+	var config struct {
+		Resources map[string]any `json:"resources"`
+	}
+	if err := json.Unmarshal(jsonBytes, &config); err != nil {
+		return nil, err
+	}
+
+	compose := DockerCompose{
+		Version:  "3",
+		Services: make(map[string]map[string]any),
+	}
+
+	// first we process postgres resources because the user and password (generated randomly) need to be created
+	// before the EC2 compose section can include them
+	for resourceName, resourceSpecs := range resources {
+		if resourceSpecs.(map[string]any)["type"] == "postgres" {
+			composeMap, err := GeneratePostgresComposeMap(resourceName, resourceSpecs.(map[string]any))
+			if err != nil {
+				return nil, err
+			}
+			compose.Services[resourceName] = composeMap
+		}
+	}
+
+	// Then process EC2 resources. This depends on the postgres user and password that were randomly
+	// generated in GeneratePostgresComposeMap above. Maybe we could just have hardcoded values for the
+	// user and password and then we wouldn't have to care about the order of operations?
+	for resourceName, resourceSpecs := range resources {
+		if resourceSpecs.(map[string]any)["type"] == "ec2" {
+			composeMap, err := GenerateEC2ComposeMap(resourceSpecs.(map[string]any))
+			if err != nil {
+				return nil, err
+			}
+			compose.Services[resourceName] = composeMap
+		}
+	}
+
+	return yaml.Marshal(compose)
+}
+
+// generate a docker compose builder for the postgres resource
+func GeneratePostgresComposeMap(dbname string, resourceFields map[string]any) (map[string]any, error) {
+	user := generateRandomString(16)
+	password := generateRandomString(16)
+
+	println("dbname", dbname)
+	println("user", user)
+	println("password", password)
+
+	// Store concrete values in composeResources
+	composeResources[dbname] = map[string]any{
+		"url":      "postgres://" + user + ":" + password + "@mydb:5432/" + dbname,
+		"user":     user,
+		"password": password,
+	}
+
+	compose := map[string]any{
+		"image": "postgres:latest",
+		"environment": map[string]string{
+			"POSTGRES_USER":     user,
+			"POSTGRES_PASSWORD": password,
+			"POSTGRES_DB":       dbname,
+		},
+		"ports": []string{"5432:5432"},
+		"healthcheck": map[string]any{
+			"test":     []string{"CMD-SHELL", fmt.Sprintf("psql -U %s -d %s -c 'SELECT 1;'", user, dbname)},
+			"interval": "5s",
+			"timeout":  "5s",
+			"retries":  5,
+		},
+	}
+
+	return compose, nil
+}
+
+func GenerateEC2ComposeMap(resourceMap map[string]any) (map[string]any, error) {
+	envVars := resourceMap["env_vars"].(map[string]string)
+	resolvedEnvVars := make(map[string]string)
+	dependencies := make([]string, 0)
+	dockerfilePath := resourceMap["dockerfile"].(string)
+	buildContext := filepath.Dir(dockerfilePath)
+	dockerfileName := filepath.Base(dockerfilePath)
+
+	// resolve references and collect dependencies
+	for k, v := range envVars {
+		ref := v
+		resolvedEnvVars[k] = resolveLocalRef(ref)
+
+		// if this env var references another resource, it's a dependency
+		parts := strings.Split(ref, ":")
+		if len(parts) == 3 && parts[0] == "ref" {
+			dependencies = append(dependencies, parts[1])
+		}
+	}
+
+	compose := map[string]any{
+		"environment": resolvedEnvVars,
+		"ports":       resourceMap["ports"],
+		"depends_on":  makeHealthcheckDependencies(dependencies),
+		"build": map[string]string{
+			"context":    buildContext,
+			"dockerfile": dockerfileName,
+		},
+	}
+
+	return compose, nil
+}
+
+func resolveLocalRef(ref string) string {
+	// ref format is "ref:resourceName:property"
+	parts := strings.Split(ref, ":")
+	if len(parts) != 3 || parts[0] != "ref" {
+		return ref
+	}
+
+	resourceName := parts[1]
+	property := parts[2]
+
+	// Check composeResources first
+	if composeRes, exists := composeResources[resourceName]; exists {
+		if val, ok := composeRes[property]; ok {
+			return val.(string)
+		}
+	}
+
+	// Then check regular resources
+	if res, exists := resources[resourceName]; exists {
+		if resourceSpecs, ok := res.(map[string]any); ok && resourceSpecs["type"] == "postgres" {
+			exposes := resourceSpecs["exposes"].(NeonPostgresExposes)
+
+			switch property {
+			case "url":
+				return exposes.Url
+			case "user":
+				return exposes.User
+			case "password":
+				return exposes.Password
+			}
+		}
+	}
+
+	// if we can't resolve it, return the original ref??
+	return ref
+}
+
+// creates condition-based dependencies that wait for health checks
+func makeHealthcheckDependencies(services []string) map[string]map[string]string {
+	deps := make(map[string]map[string]string)
+	for _, service := range services {
+		deps[service] = map[string]string{
+			"condition": "service_healthy",
+		}
+	}
+	return deps
+}
+
+func generateRandomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyz"
+	b := make([]byte, length)
+
+	for i := range b {
+		n, _ := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		b[i] = charset[n.Int64()]
+	}
+
+	return string(b)
 }
